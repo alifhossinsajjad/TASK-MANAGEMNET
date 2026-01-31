fbea53824e0820a97544cb3e38bdd59a1709efb5
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-31 20:21:53 +0600

    update the store file

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,38 +1,10 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./features/counter/counterSlice";
-import todoReducer from "./features/todo/todoSlice";
-import userReducer from "./features/user/userSlice";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
 
-const reducers = combineReducers({
-  counter: counterReducer,
-  user: userReducer,
-  todo: todoReducer,
-});
+export const store = configureStore ({
+  reducer: {},
+})
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+export type RootState = ReturnType<typeof store.getState>
 
-export const store = configureStore({
-  reducer: persistedReducer,
-});
-
-// export const store = configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//     todo: persistedReducer,
-//     user: userReducer,
-//   },
-// });
-
-export const persistedStore = persistStore(store);
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
